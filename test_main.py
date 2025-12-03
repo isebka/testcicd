fd72a4a90dc25820514398a5d87da1089abf536d
ZeroTop <alekseyyyrevich@mail.ru>
2025-12-03 10:35:07 +0000

    add index.html and adjustment ci/cd

--- test_main.py
+++ test_main.py
@@ -1,5 +1,10 @@
-from main import add
+import os
 
-def test_add():
-    assert add(2, 2) == 5
-    assert add(1, 5) == 6
+def test_index_html_exists():
+    assert os.path.exists("index.html"), "Файл index.html не найден!"
+
+def test_index_html_content():
+    with open("index.html", "r", encoding="utf-8") as f:
+        content = f.read()
+        assert "<html>" in content, "В файле нет тега <html>"
+        assert "<body>" in content, "В файле нет тега <body>"
